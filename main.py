83212a3457b3733f0fe31f7e3594e20a2ad96159
jiasunm <jiasunm@amazon.com>
2024-03-10 12:15:55 +0800

    Fixed the bug in calling functions.

--- main.py
+++ main.py
@@ -5,12 +5,10 @@ from tools.utils import Utils
 Utils.logger.add("managed_scaling_enhanced.log",
                  format="{time} {level} {message}", level="DEBUG")
 
-managed_scaling_enhanced_client = ManagedScalingEnhanced()
+managed_scaling_enhanced_client = ManagedScalingEnhanced(emr_id='j-1F74M1P9SC57B', prefix="managedScalingEnhanced")
 
 # 示例调用，这里需要替换为实际参数
 if __name__ == '__main__':
 
-    scaleStatus = managed_scaling_enhanced_client.determine_scale_status(
-        emr_id='j-1F74M1P9SC57B', prefix="managedScalingEnhanced"
-    )
+    scaleStatus = managed_scaling_enhanced_client.determine_scale_status()
     Utils.logger.info(f"Scale Status: {scaleStatus}")
